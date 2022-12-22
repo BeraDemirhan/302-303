ea51996e20851131637cdc2d10e7c6800fcba6a9
furknozg <furkanozgultekin@gmail.com>
2022-12-22 19:26:27 +0300

    alien iface template

--- EscapeFromKoc/src/main/java/Backend/GameObjects/Aliens/Alien.java
+++ EscapeFromKoc/src/main/java/Backend/GameObjects/Aliens/Alien.java
@@ -1,5 +1,27 @@
 package Backend.GameObjects.Aliens;
 
-public interface Alien {
+import Backend.GameObjects.GameObjectIntterface;
+import Backend.Player.Player;
 
+import javax.swing.*;
+
+public interface Alien extends GameObjectIntterface {
+    @Override
+    void spawnObject(int x, int y);
+
+    @Override
+    JLabel getObjectLabel();
+
+
+    @Override
+    int getX();
+
+    @Override
+    int getY();
+
+    int setX(int x);
+
+    int setY(int y);
+
+    void applyAlienGoal(Player p);
 }
