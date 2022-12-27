6befeb935a3652095305b50410383f20cb5aca1f
Bera Demirhan <56884682+BeraDemirhan@users.noreply.github.com>
2022-12-27 13:34:38 +0300

    Blind Alien Sprite added, bug fix for name password txt not found

--- EscapeFromKoc/src/main/java/Backend/GameObjects/Aliens/BlindAlienImpl.java
+++ EscapeFromKoc/src/main/java/Backend/GameObjects/Aliens/BlindAlienImpl.java
@@ -1,52 +1,42 @@
 package Backend.GameObjects.Aliens;
 
 import Backend.Player.Player;
+import UI.UIUtils;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class BlindAlienImpl implements Alien{
+public class BlindAlienImpl implements Alien {
 
     private boolean ALERT = false;
     private int velocity = 5;
     private int damage = 1;
     private int x;
     private int y;
-    private Image blindAlienImg = new ImageIcon("EscapeFromKoc/resources/rabbit-front-angled.png").getImage();
-    private JLabel blindAlienLabel;
-    public BlindAlienImpl(int x , int y ){
+
+
+    public BlindAlienImpl(int x, int y) {
         spawnObject(x, y);
     }
 
     @Override
-    public void spawnObject(int x, int y){
+    public void spawnObject(int x, int y) {
         this.x = x;
         this.y = y;
-
-        blindAlienImg = blindAlienImg.getScaledInstance(54, 96, Image.SCALE_SMOOTH);
-        blindAlienLabel = new JLabel(new ImageIcon(blindAlienImg));
-        blindAlienLabel.setBounds(x, y, 100, 100);
-    }
-
-
-
-    public Image getBlindAlienImg() {
-        return blindAlienImg;
     }
 
     @Override
-    public JLabel getObjectLabel(){
-        return blindAlienLabel;
+    public JLabel getObjectLabel() {
+        return UIUtils.createLabel("EscapeFromKoc/resources/BlindAlien/BlindAlienFront.png", x, y, 96, 54);
     }
 
-
     @Override
-    public int getX(){
+    public int getX() {
         return this.x;
     }
 
     @Override
-    public int getY(){
+    public int getY() {
         return this.y;
     }
 
@@ -58,7 +48,7 @@ public class BlindAlienImpl implements Alien{
         this.y = y;
     }
 
-    public void moveAlienToPlayer(Player p){
+    public void moveAlienToPlayer(Player p) {
         if (y < p.getY()) {
             this.setY(y + velocity);
             this.setX((int) (y - velocity * ((float) (430 - x) / (y + 772))));
@@ -75,18 +65,20 @@ public class BlindAlienImpl implements Alien{
         }
 
     }
-    public void attackPlayer(Player p){
+
+    public void attackPlayer(Player p) {
         p.addHealth(-damage);
     }
 
-    public void applyAlienGoal(Player p){
+    public void applyAlienGoal(Player p) {
         // alien goals are intended to be defined for all aliens,
         // eg: blind alien will try to move towards player on movement events
-        // (applying the goal is only the end result not the conditions that it has to have to apply it)
-        if(Math.abs(p.getX() - x) < 50
-                && Math.abs(p.getY() - y) < 50){
+        // (applying the goal is only the end result not the conditions that it has to
+        // have to apply it)
+        if (Math.abs(p.getX() - x) < 50
+                && Math.abs(p.getY() - y) < 50) {
             attackPlayer(p);
-        }else{
+        } else {
             moveAlienToPlayer(p);
         }
     }
@@ -96,6 +88,5 @@ public class BlindAlienImpl implements Alien{
         // Placeholder code
         return false;
     }
-
 
 }
