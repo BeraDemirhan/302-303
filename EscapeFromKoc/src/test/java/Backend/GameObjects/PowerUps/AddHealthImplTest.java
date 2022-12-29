51edc97afd91f9ec373b7b1deeff99b181f454bb
Bera Demirhan <56884682+BeraDemirhan@users.noreply.github.com>
2022-12-29 18:35:25 +0300

    Added Tests for Health Impl

--- EscapeFromKoc/src/test/java/Backend/GameObjects/PowerUps/AddHealthImplTest.java
+++ EscapeFromKoc/src/test/java/Backend/GameObjects/PowerUps/AddHealthImplTest.java
@@ -12,10 +12,32 @@ public class AddHealthImplTest {
 
     @org.junit.jupiter.api.Test
     void addHealth() throws IOException {
-        Player player = new Player();
+        Player player = Player.getPlayer();
         int prev = player.getHealth();
         player.addHealth(1);
         assertEquals(prev + 1, player.getHealth());
-
     }
+
+    @org.junit.jupiter.api.Test
+    void spawnHealth() throws IOException {
+        AddHealthImpl addHealth = new AddHealthImpl(0, 0);
+        assertEquals(0, addHealth.getX());
+        assertEquals(0, addHealth.getY());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHealth() throws IOException {
+        AddHealthImpl addHealth = new AddHealthImpl(0, 0);
+        assertNotNull(addHealth.getHealth());
+    }
+
+    @org.junit.jupiter.api.Test
+    void activatePowerUp() throws IOException {
+        Player player = Player.getPlayer();
+        int prev = player.getHealth();
+        AddHealthImpl addHealth = new AddHealthImpl(0, 0);
+        addHealth.activatePowerUp(player);
+        assertEquals(prev + 1, player.getHealth());
+    }
+
 }
