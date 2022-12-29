9cb4a0d83e4f38ae25729f36cfc8cdb8200a6d72
Bera Demirhan <56884682+BeraDemirhan@users.noreply.github.com>
2022-12-29 18:07:49 +0300

    login test try 3

--- EscapeFromKoc/src/test/java/Backend/ButtonRespondersTest.java
+++ EscapeFromKoc/src/test/java/Backend/ButtonRespondersTest.java
@@ -1,15 +1,21 @@
+package Backend;
+
 import Backend.ButtonResponders;
 import Backend.GameControler;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
 
 class ButtonRespondersTest {
 
-    @Test
-    void login() {
+    @org.junit.jupiter.api.Test
+    void login() throws IOException {
         ButtonResponders buttonResponders = new ButtonResponders();
         buttonResponders.SignUpButton("username", "password");
-        assertTrue(buttonResponders.LoginButton("username", "password"), true);
+        assertTrue(buttonResponders.LoginButton("username", "password"));
     }
 
 }
