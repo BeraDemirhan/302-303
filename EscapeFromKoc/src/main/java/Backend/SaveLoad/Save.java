2e82aae29ea1c617731931b65e00c085d0d96e02
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 <furkanozgultekin@gmail.com>
2023-01-20 02:55:15 +0300

    Added mongodb save option (note: Works as default)

--- EscapeFromKoc/src/main/java/Backend/SaveLoad/Save.java
+++ EscapeFromKoc/src/main/java/Backend/SaveLoad/Save.java
@@ -7,9 +7,21 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import Backend.GameControler;
 import Backend.GameObjects.GameObjectIntterface;
+import Backend.Player.Inventory;
+import Backend.Player.Player;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
 
 public class Save {
     private static int saveNumber = 1;
@@ -17,7 +29,19 @@ public class Save {
     private static File NamesandPasswords = new File("EscapeFromKoc/resources/NamesPasswords.txt");
     private static PrintWriter pw = null;
 
-    private static void pwInit(){
+    private static String saveMethod = "MongoDB";
+
+    public static void setSaveMethod(boolean mongo) {
+        if (mongo) {
+            saveMethod = "MongoDB";
+        } else {
+            saveMethod = "Plaintext";
+        }
+
+    }
+
+    private static void pwInit() {
+
         try {
             pw = new PrintWriter(saveFile);
         } catch (FileNotFoundException ex) {
@@ -25,48 +49,91 @@ public class Save {
         }
     }
 
+    private static MongoDatabase mongoInit() {
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        MongoCredential credential = MongoCredential.createCredential("sampleUser", "myDb",
+                "password".toCharArray());
+        return mongo.getDatabase("myDb"); 
 
-    //Save the game, including the player's current position, inventory, health, current level, location of the objects in the level, etc.
+    }
 
+    private static void saveToDB(){
+        MongoDatabase database = mongoInit();
+        String collName = "Level"+GameControler.getCurrentLevel();
+
+        database.createCollection(collName);
+        MongoCollection<Document> collection = database.getCollection(collName);
+  
+        Document PlayerDocument = new Document("title", "Player")
+        .append("coordinates", new ArrayList<Integer>(Arrays.asList(new Integer[]{GameControler.getPlayerCoords()[0], GameControler.getPlayerCoords()[1]})).toString() )
+        .append("inventory",  new ArrayList<String>( Arrays.asList(Player.getPlayer().getInventoryStr().split(" ", 0))).toString() )
+        .append("health", GameControler.getPlayerHealth());
+        
+        collection.insertOne(PlayerDocument);
+
+        
+
+        int o = 0;
+        for (String i : GameControler.getObjects()) {
+            int[] coords = GameControler.getObjectCoords(i);
+            Document ObjectDocument = new Document("id", o);
+            ObjectDocument.append("title", "Object").append("coordinates", new ArrayList<Integer>(Arrays.asList(new Integer[]{coords[0], coords[1]})).toString());
+            o++;
+            collection.insertOne(ObjectDocument);
+        }
+        
+        //Inserting document into the collection
+        
+    }
+
+    // Save the game, including the player's current position, inventory, health,
+    // current level, location of the objects in the level, etc.
 
     public static void saveGame(){
         //Save the game, including the player's current position, inventory, health, current level, location of the objects in the level, etc.
         System.out.println("Saving game...");
-        pwInit();
-        saveLevel();
-        savePlayer();
-        saveObjects();
-        pw.close();
+        if(saveMethod.equals("Plaintext")){
+            pwInit();
+            saveLevel();
+            savePlayer();
+            saveObjects();
+            pw.close();
+
+        }else if (saveMethod.equals("MongoDB")){
+            saveToDB();
+        }
+
         System.out.println("Game saved!");
     }
 
-    public static void setSaveNum(){
-        //Set the save number to the next available save slot
+    public static void setSaveNum() {
+        // Set the save number to the next available save slot
         int i = 1;
-        while(true){
+        while (true) {
             saveFile = new File("EscapeFromKoc/resources/Save" + i + ".txt");
-            if(saveFile.exists()){
+            if (saveFile.exists()) {
                 i++;
-            }else{
+            } else {
                 saveNumber = i;
                 break;
             }
         }
     }
 
-    public static void write(String text){
+    public static void write(String text) {
 
         pw.println(text);
 
     }
-    public static void saveLevel(){
-        //Save the current level
+
+    public static void saveLevel() {
+        // Save the current level
         int level = GameControler.getCurrentLevel();
         write("Level: " + level);
     }
 
-    public static void savePlayer(){
-        //Save the player's current position, inventory, health, etc.
+    public static void savePlayer() {
+        // Save the player's current position, inventory, health, etc.
         int[] coords = GameControler.getPlayerCoords();
         write("Player's coordinates: " + coords[0] + ", " + coords[1]);
         String inventory = GameControler.getPlayerInventory();
@@ -75,18 +142,18 @@ public class Save {
         write("Player's health: " + health);
     }
 
-    public static void saveObjects(){
-        //Save the location of the objects in the level
+    public static void saveObjects() {
+        // Save the location of the objects in the level
         write("Objects:");
-        for(String i : GameControler.getObjects()){
+        for (String i : GameControler.getObjects()) {
             int[] coords = GameControler.getObjectCoords(i);
             write("Object " + i + ": " + coords[0] + ", " + coords[1]);
         }
-        
+
     }
 
-    public ArrayList<String> read(File file){
-        //Read the file
+    public ArrayList<String> read(File file) {
+        // Read the file
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(file));
@@ -107,10 +174,10 @@ public class Save {
         return lines;
     }
 
-    public void saveNamesandPasswords(){
-        //Save the names and passwords of the users
+    public void saveNamesandPasswords() {
+        // Save the names and passwords of the users
         write("Names and passwords:");
         write(read(NamesandPasswords).toString());
     }
-    
+
 }
